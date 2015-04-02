46d81d4d70eae59ddcb3e48a72e084994cf9a8e1
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-04-02 18:53:07 -0400

    Week 1, Goal 1, Numbers Arrays Example 1
    Using the array provided to see what is contained in each index of the
    array by outputting to the console.

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -109,25 +109,25 @@ console.log("Basic Math: ", + counter);
 
     //quick operations with "assignment operator" +=, -=, *=, /=, %=
     //one example below, can show more examples with other assignment operators
-var num1 = 10;
-var num2 = 5;
-num1 += num2;
-console.log("+=: ", num1);
+//var num1 = 10;
+//var num2 = 5;
+//num1 += num2;
+//console.log("+=: ", num1);
 
     //increment or decrement numbers by 1 with ++ or --
-var myNum = 5;
-console.log("myNum = ", myNum);
+//var myNum = 5;
+//console.log("myNum = ", myNum);
 
-myNum++
-console.log("myNum = ", myNum);
+//myNum++
+//console.log("myNum = ", myNum);
 
-myNum--
-console.log("myNum = ", myNum);
+//myNum--
+//console.log("myNum = ", myNum);
 
     //this decrements right before it is being used
-var num = 10;
---num;
-console.log("-- counter: ", num); 
+//var num = 10;
+//--num;
+//console.log("-- counter: ", num); 
 
     //this decrements after it is used  
 //num--;
@@ -135,25 +135,25 @@ console.log("-- counter: ", num);
 
     //comment out the above example
     //this decrements after it is being used as well
-console.log("counter --: ", num--);
-console.log("counter --: ", num);
+//console.log("counter --: ", num--);
+//console.log("counter --: ", num);
 
     //adding a number to a string always equals a string
-var myStr = "6" + 2;
-console.log("Number + Strings: ",  myStr);
+//var myStr = "6" + 2;
+//console.log("Number + Strings: ",  myStr);
 
     //other math operations will result in a number
-var myStr = "6" / 2; 
-console.log("Number / Strings: ", myStr);
+//var myStr = "6" / 2; 
+//console.log("Number / Strings: ", myStr);
 
     //order of operations: ( ), *, /, +, -
-var num = 4 + (6 * 5 - 10) / 5;
-console.log("Order of Operations: ", num);
+//var num = 4 + (6 * 5 - 10) / 5;
+//console.log("Order of Operations: ", num);
 
     //modulo example
-var num6 = 10;
-var num7 = 3;
-console.log("Remainder: ", num6 % num7);
+//var num6 = 10;
+//var num7 = 3;
+//console.log("Remainder: ", num6 % num7);
 
 // arrays: (definition, indexing, getter/setter)
 console.log('------ arrays ----------');
@@ -180,7 +180,10 @@ console.log('------ arrays ----------');
  */
 
     //arrays can hold any data type
-
+console.log("Index 0: ", myArr[0]);
+console.log("Index 1: ", myArr[1]);
+console.log("Index 2: ", myArr[2]);
+console.log("Index 3: ", myArr[3]);
 
 
  /*******************************************
