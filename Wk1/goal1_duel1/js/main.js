004185cee5fb1d8834ee9d07e29fbc768d81d92e
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-04-05 05:42:06 -0400

    Week 1 Homework 1 Duel1 Building Functions
    Building up each function for the fighting game itself. Set up loop to
    test amount of rounds, which player is the winner or loser, run the
    actual program itself.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -24,19 +24,66 @@
 	//What round we are in 
 	var round = 1;
 	
-	//Fight Function
+	//Fight Function - i for initialize
 	function fight(){
 		//fight code
+		console.log("Fight Function Starts Here!");
+		
+		alert(playerOneName + ":" + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
+		
+		for (var i=0; i<10; i++){
+			//random formula is: Math.floor(Math.random() * (max - min) + min);
+			var minDamage1 = player1Damage * .5;
+			var minDamage2 = player2Damage * .5;
+			
+			var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+			var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
+			
+			//Damage is inflicted
+			playerOneHealth -= f1;
+			playerTwoHealth -= f2;
+			
+			console.log(playerOneName+":"+playerOneHealth+playerTwoName+":"+playerTwoHealth);
+			
+			var results = winnerCheck();
+			console.log(results);
+			
+			if(results === "No Winner!"){
+				
+				round++;
+				alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER* " + playerTwoName + ":" + playerTwoHealth);
+			}else{
+				alert(results);
+			};
+		};
 		
 	};
 	
 	//Winner Check Function
 	function winnerCheck(){
+	console.log("Inside winnerCheck Function");
 		//winner code
+		var result = "No Winner!";
 		
+		if(playerOneHealth < 1 && playerTwoHealth < 1){
+			
+			result = "You Both Die.";
+			
+		}else if(playerOneHealth < 1){
+			
+			result = playerTwoName + "Wins!"
+			
+		}else if(playerTwoHealth < 1){
+			
+			result = playerOneName + "Wins!"
+			
+		};
+		
+		return result;
 	};
 	
 	//Fight Function Call - program begins here 
+	console.log("Program Starts Here!");
 	fight();
 	
 })();
