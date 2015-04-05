75684a6bb3a3bd71a8602a85689dea8f68e70376
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-04-05 04:44:03 -0400

    Week 1 Homework Duel1 Variables
    Set up variables for week 1 homework Duel1

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -8,5 +8,17 @@
 //self-excecuting function
 (function(){
 	console.log("Fight!");
+	
+	//Player Names
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
+	
+	//Player Damage
+	var player1Damage = 20;
+	var player2Damage = 20;
+	
+	//Player Health
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
 })();
  
