a5daa61f55212306ba9b5dd121b3a50a2402568b
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-05-07 20:15:54 -0400

    Week 1 Activity 1 Numbers
    Completed

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -74,11 +74,11 @@ console.log('------ variables ----------');
 console.log('------ strings ----------');
 
     //basic string
-		var name = "James Bond"; 
+		var name = "James Bond!"; 
 		console.log(name);
 
     //string w/ escape characters " \ "
-		var testString0 = "He's in PWA1"; 
+		var testString0 = "He's in PWA1."; 
 		console.log("Example 1: ", testString0);
 
     //string w/ escape characters and mixed single/double quotes
@@ -86,7 +86,7 @@ console.log('------ strings ----------');
 		console.log("Example 2: ", testString1);
 
     //this example shows that all the "phase" vars above were overwritten
-		var testString2 = 'He\'s in PWA1'; 
+		var testString2 = 'He\'s in PWA1.'; 
 		console.log("Example 3: ", testString2);
 
 
@@ -97,33 +97,59 @@ console.log('------ numbers ----------');
 
 
     //basic math
+		var counter = 10;
+		console.log("Basic Math: ", + counter + 1);
+		console.log("Basic Math: ", + counter); 
 
 
     //quick operations with "assignment operator" +=, -=, *=, /=, %=
     //one example below, can show more examples with other assignment operators
-
+		var num1 = 10;
+		var num2 = 5;
+		num1 += num2;
+		console.log("+=: ", num1);
 
     //increment or decrement numbers by 1 with ++ or --
+		var myNum = 5;
+		console.log("myNum = ", myNum);
+		
+		myNum++
+		console.log("myNum = ", myNum);
+		
+		myNum--
+		console.log("myNum = ", myNum);
+
 
     //this decrements right before it is being used
-
+		var num = 10;
+		--num;
+		console.log("-- Counter: ", num); 
+		
     //this decrements after it is used
-
+		num--;
+		console.log("Counter --: ", num);
+		
     //comment out the above example
     //this decrements after it is being used as well
-
+		console.log("Counter --: ", num--);
+		console.log("Counter --: ", num);
 
     //add a number to a string is always a string
-
+		var myStr = "6" + 2;
+		console.log("Number + Strings: ",  myStr);
 
     //other math operations will result in a number
-
+		var myStr = "6" / 2; 
+		console.log("Number / Strings: ", myStr);
 
     //order of operations: ( ), *, /, +, -
-
+		var num = 4 + (6 * 5 - 10) / 5;
+		console.log("Order of Operations: ", num);
 
     //modulo example
-
+		var num6 = 10;
+		var num7 = 3;
+		console.log("Remainder: ", num6 % num7);
 
 // arrays: (definition, indexing, getter/setter)
 console.log('------ arrays ----------');
