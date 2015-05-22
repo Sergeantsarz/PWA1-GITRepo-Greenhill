a2a8d0fa04fcb8a01065aef63dbcbb2921462df7
Sarah Greenhill <sarah@sarahgreenhill.com>
2015-05-21 23:13:29 -0400

    Week 3 Homework Setup

---
+++ Wk3/goal3_assign_debug_v2/js/search.js
@@ -0,0 +1,141 @@
+/*
+     Name: Sarah Greenhill
+     Date: 05/21/15
+     Class & Section: PWA1
+     Comments: Week 3 Homework Debug Search v2
+ */
+
+// Create privatized scope using a self-executing function
+(function(){
+	console.log("Inside initial function");
+	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
+	var resultsDIV = document.getElementById("results"),
+		searchInput = document.forms[0].search,
+		currentSearch = '';
+	
+	// Validates search query
+	//There are two equal signs here when there should be one
+	var validqte = function(query){
+	console.log("3");
+		// Trim whitespace from start and end of search query
+		while(validqte.charAt(0) === " "){
+			validqte = validqte.substring(1, validqte.length);
+		}; 
+		//beginning parenthesis is missing 
+		while(validqte.charAt(validqte.length) === ""){
+			validqte = validqte.substring(1, validqte.length-1);
+		//No closing bracket for while loop here 
+		};
+
+	}; //Missing close bracket for validqte function
+		
+		// Check search length, must have 3 characters
+		//If the query to the search engine is less than 3, it will alert the user that what they entered is too short and to try again with a longer entry
+		if(validqte.length < 3){
+		console.log("4");
+		//missing end quotes, added them in 
+		//alert the user that their search was not enough characters 
+			alert("Your search query is too small, try again.");
+			 
+			// (DO NOT FIX THE LINE DIRECTLY BELOW)
+			searchInput.focus();
+			//return the results of the search 
+			return;
+		};
+		console.log("5");
+		//if the length of the query is sufficient, then search for the term entered by the user 
+		search(query);
+	
+	
+		// Finds matches when the user searches for something
+		var search = function(query){
+		console.log("6");
+		// split the user's search query string into an array
+		var queryArray = search.join(" ");
+		
+		// array to store matched results from database.js
+		var results = [];
+
+		// loop through each index of db array
+		for(var i=0, j=db.length; i<j; i++){
+		console.log("7");
+			// each db[i] is a single video item, each title ends with a pipe "|"
+			var dbTitleEnd = db[i].indexOf('|');
+			// save a lowercase variable of the video title by switching anything the user enters and sending it to all lower case
+			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+			
+			// loop through the user's search query words
+			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
+			// save a lowercase variable of the search keyword
+				var qitem = queryArray[ii].tolowercase();
+				
+				// is the keyword anywhere in the video title?
+				// If a match is found, push full db[i] into results array
+				//The results array will now contain anything that querys a result based upon what the user has entered into the search 
+				var compare = dbitem.indexOf(qitem);
+				if(compare !== -1){
+					results.push(db[i]);
+				}; //Missing end curly brace for search function
+			}; //Missing end curly brace for db array loop
+		}; //Missing end curly brace for db for loop
+		
+		results.sort();
+		
+		// Check that matches were found, and run output functions
+		//if the results =  0 then let the user know there was no match by initalizing the noMatch function, else show the user what matched their query and output those matches to the results variable 
+		if(results.length = 0){
+			noMatch();
+		}else{
+			showMatches(results);
+		};
+	};
+	
+	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
+	//noMatch function is defined here. If no matches are found in the database, display these HTML properties to the user
+	var noMatch = function(){
+		var html = ''+
+			'<p>No Results found.</p>'+
+			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
+		;
+		resultsDIV.innerHTML = html;
+	};
+	
+	// Put matches into page as paragraphs with anchors
+	var showMatches = function(results){
+		
+		// THE NEXT 4 LINES ARE CORRECT.
+		var html = '<p>Results</p>', 
+			title, 
+			url
+		;
+		
+		// loop through all the results search() function
+		for(var i=0, j=results.length; i<j; i++){
+		
+			// title of video ends with pipe
+			// pull the title's string using index numbers
+			titleEnd = results[i].indexOf('|');
+			title = results[i].subString(0, titleEnd);
+			
+			// pull the video url after the title
+			url = results[i].substring(results[i].indexOf('|')+1, results[i].length);
+			
+			// make the video link - THE NEXT LINE IS CORRECT.
+			html += '<p><a href=' + url + '>' + title + '</a></p>';
+		};
+		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+	};
+	
+	// The onsubmit event will be reviewed in upcoming Course Material.
+	// THE LINE DIRECTLY BELOW IS CORRECT
+	document.forms[0].onsubmit = function(){
+		var query = searchInput.value;
+		//validate is spelled incorrectly 
+		validqte(query);
+
+        // return false is needed for most events - this will be reviewed in upcoming course material
+        // THE LINE DIRECTLY BELOW IS CORRECT
+		return false;
+	}; //Missing end curly brace for document.forms function
+
+})();
---
+++ Wk3/goal3_assign_duel3/js/main.js
@@ -0,0 +1,84 @@
+/*
+     Name: Sarah Greenhill
+     Date: 05/21/15
+     Class & Section: PWA1
+     Comments: Week 3 Homework Duel3 
+ */
+ 
+//This is a self-excecuting function
+(function(){
+	console.log("Fight!");
+	
+	//These are the player's names, damage, and health contained inside an array 
+	var playerOneArr = ["Spiderman", 20, 100];
+	var playerTwoArr = ["Batman", 20, 100];
+	
+		//This variable shows what round we're in
+	var round = 0;
+	
+	//This is the fight function, I stands for Initialize (as in initialize the function)
+	function fight(){
+		//Inside the fight function
+		console.log("Fight Function Starts Here!");
+		//This displays an alert to the user showing the player's names, current health, and that the round has started
+		alert(playerOneArr[0] + ": " + playerOneArr[2] + " *START* " + playerTwoArr[0] + ": " + playerTwoArr[2]);
+		
+		for (var i=0; i<10; i++){
+			//Formula for random generated number: Math.floor(Math.random() * (max - min) + min); These are variables set up to contain the math done to determine the random amount of damage that each player does
+			var minDamage1 = playerOneArr[1] * .5; //This is the minimum amount of damage player 1 dishes out
+			var minDamage2 = playerTwoArr[1] * .5; //This is the minimum amount of damage player 2 dishes out 
+			
+			var f1 = Math.floor(Math.random() * (playerOneArr[1] - minDamage1) + minDamage1); //Random math formula calculating dmg for player 1 
+			var f2 = Math.floor(Math.random() * (playerTwoArr[1] - minDamage2) + minDamage2); //Random math formula calculating dmg for player 2
+			
+			//This is where damage is done by each player
+			playerOneArr[2] -= f1;
+			playerTwoArr[2] -= f2;
+			//This logs to the console the current health of each player
+			console.log(playerOneArr[0] + ": " + playerOneArr[2] + playerTwoArr[0] + ": " + playerTwoArr[2]);
+			
+			var results = winnerCheck(); //This calls the winnerCheck function where it will check to see if a player has bested the other, or if both have died 
+			console.log(results); //Logs the variable 'results' to the console showing who's been killed and/or what their health is 
+			
+			//This if-else states that if there's no winner, move on to the next round, add 1 to the round total, then alert the user of the player's current health totals and round number. If someone has won, alert the user who won or if both players have died 
+			if(results === "No Winner!"){
+				
+				round++;
+				alert(playerOneArr[0] + ": " + playerOneArr[2] + " *ROUND " + round + " OVER* " + playerTwoArr[0] + ": " + playerTwoArr[2]);
+			}else{
+				alert(results);
+				break;
+			}; //End of the for loop
+		}; //End of the if-else statement
+		
+	}; //End of the fight function
+	
+	//This function checks who the winner is 
+	function winnerCheck(){
+	console.log("Inside winnerCheck Function"); //This logs to the console that the winnerCheck function is currently happening 
+		//Inside the winner check function
+		var result = "No Winner!"; //Default state of 'result' variable is that ther is no winner
+		//If either player's health has gone lower than 1, result variable will return that both players have died to the result variable, which will alert the user
+		if(playerOneArr[2] < 1 && playerTwoArr[2] < 1){
+			
+			result = "You Both Die.";
+			
+		}else if(playerOneArr[2] < 1){
+			
+			result = playerTwoArr[0] + " Wins!"
+			
+		}else if(playerTwoArr[2] < 1){
+			
+			result = playerOneArr[0] + " Wins!"
+			
+		}; //Ends the if-else for winnerCheck 
+		
+		return result; //Returns the results of the winner check
+	}; //End of the winnerCheck Function
+	
+	//This calls the fight function into action - everything starts here 
+	console.log("Program Starts Here!");
+	fight();
+	
+})();
+ 
